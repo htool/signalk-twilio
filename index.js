ad177f83fae8b774940083b799de31ad007f4a72
Albert Smit <albertsmit93@gmail.com>
2020-03-04 11:59:50 +0100

    first version of sms notification plugin

---
+++ index.js
@@ -0,0 +1,98 @@
+const twilio = require('twilio');
+const PLUGIN_ID = 'signalk-twilio';
+const PLUGIN_NAME = 'Signalk twilio';
+var unsubscribes = [];
+module.exports = function(app) {
+  var plugin = {};
+
+  plugin.id = PLUGIN_ID;
+  plugin.name = PLUGIN_NAME;
+  plugin.description = 'A plugin to send sms notifications when an event occurs';
+
+  plugin.start = function(options, restartPlugin) {
+    app.debug('Plugin started');
+    plugin.options = options;
+
+    let accountSid = options.account.Sid;
+    let authToken = options.account.token;
+    let from = options.account.from;
+    let client = twilio(accountSid, authToken);
+    options.notifications.forEach(option => listen(option, client, from));
+
+    app.setProviderStatus('Running');
+
+
+  };
+
+  function listen(option, client, from) {
+    let _notify = function(event) {
+      client.messages
+        .create({
+          body: `Alert from code kilo: ${option.message}`,
+          from: from,
+          to: option.to
+        })
+        .then(message => console.log(message.sid));
+    };
+    app.on(option.event, _notify);
+    unsubscribes.push(() => {
+      app.removeListener(option.event, _notify);
+    });
+  }
+
+
+  plugin.stop = function() {
+    // Here we put logic we need when the plugin stops
+    app.debug('Plugin stopped');
+    unsubscribes.forEach(f => f());
+    app.setProviderStatus('Stopped');
+  };
+
+  plugin.schema = {
+    title: PLUGIN_NAME,
+    type: 'object',
+    properties: {
+      account: {
+        type: 'object',
+        properties: {
+          Sid: {
+            type: 'string',
+            title: 'Account Sid'
+          },
+          token: {
+            type: 'string',
+            title: 'Token'
+          },
+          from: {
+            type: 'string',
+            title: 'Sender'
+          }
+        }
+      },
+      notifications: {
+        type: 'array',
+        title: 'notifications',
+        items: {
+          type: 'object',
+          properties: {
+            event: {
+              type: 'string',
+              title: 'event'
+            },
+            message: {
+              type: 'string',
+              title: 'message'
+            },
+            to: {
+              type: 'string',
+              title: 'to'
+            }
+          }
+        }
+      }
+    }
+
+  };
+
+  return plugin;
+};
